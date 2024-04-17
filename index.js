265f7ba1722a7fef885ed9f0d2d3a0d488f430eb
syah hasrizal <b022220066@student.utem.edu.my>
2024-04-17 22:56:58 +0800

    hehe

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3004;
 
 app.use(express.json())
 
@@ -11,3 +11,28 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
    console.log(`Example app listening on port ${port}`)
 })
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://b022220066:Oxoby5599-@class.tdi4cxs.mongodb.net/?retryWrites=true&w=majority&appName=CLASS";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    let result = await client.db("Mlbb").collection('Heroes').find({name: 'Belerick',role:'Tank'}).toArray();
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
